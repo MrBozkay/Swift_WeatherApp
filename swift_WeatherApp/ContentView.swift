b790b1428fd9de045469f89dae7aafb8dff78a69
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-11 09:35:31 +0300

    Initial Commit

---
+++ swift_WeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_WeatherApp
+//
+//  Created by mustafa bozkaya on 11.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_WeatherApp/swift_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_WeatherAppApp.swift
+//  swift_WeatherApp
+//
+//  Created by mustafa bozkaya on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
